fa82c64d958df5509713da75a0cbe2acb10b6550
Ryan_windows <Ryan_windows@test.com>
2025-04-26 16:15:23 +0800

    去除test.py文件

--- test.py
+++
@@ -1,6 +0,0 @@
-from schedule_manager import ScheduleManager
-
-sm = ScheduleManager()
-schedule = sm.generate_monthly_schedule()
-sm.save_monthly_schedule(schedule)
-print(sm.should_send_email_today('2025-04-29'))
